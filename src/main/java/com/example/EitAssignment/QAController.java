e33d474b5ca88b5d66635be3ec1f9138766b2f61
giashuddin <mdgiashu069@gmail.com>
2021-01-15 12:20:33 +0600

    Update

--- src/main/java/com/example/EitAssignment/QAController.java
+++ src/main/java/com/example/EitAssignment/QAController.java
@@ -25,6 +25,11 @@ public class QAController {
         return qaService.basicWorldAffairs(question);
     }
 
+    @GetMapping("/world/affairs")
+    public Object basicWoAffairs(@RequestParam("q") String question) {
+        return qaService.basicWorldAffairs(question);
+    }
+
     @GetMapping("/weather")
     public Object getWeather(@RequestParam("q") String question) {
         return qaService.getWeather(question);
